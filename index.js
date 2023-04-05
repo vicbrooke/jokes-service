d435ef249927c6b959db45ab3602c238d0e87bb2
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-04-05 11:54:58 +0100

    Completed exercise 1, tests passing

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const express = require('express');
+const express = require("express");
+const { Op } = require("sequelize");
 const app = express();
-const { Joke } = require('./db');
+const { Joke } = require("./db");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes', async (req, res, next) => {
+app.get("/jokes", async (req, res, next) => {
+  if (req.query) {
+    try {
+      const jokes = await Joke.findAll({
+        where: {
+          tags: {
+            [Op.substring]: req.query.tags ?? "",
+          },
+          joke: {
+            [Op.substring]: req.query.content ?? "",
+          },
+        },
+      });
+      res.send(jokes);
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+    return;
+  }
+
   try {
-    // TODO - filter the jokes by tags and content
-    const jokes = [];
+    const jokes = await Joke.findAll();
     res.send(jokes);
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
